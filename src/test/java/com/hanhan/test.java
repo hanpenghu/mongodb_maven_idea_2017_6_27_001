22f797fae4f7afc970a796027e1da4a57265f525
hanpenghu <563909408@qq.com>
2017-06-27 16:24:12 +0800

    2017_6_27   weekday(2)   16:24:04

---
+++ src/test/java/com/hanhan/test.java
@@ -0,0 +1,16 @@
+package com.hanhan;
+
+/**
+ * Created by Administrator on 2017/6/27.
+ */
+public class test {
+
+    public void f(){
+
+    }
+
+
+    public static void main(String[]args){
+        new test().f();
+    }
+}
