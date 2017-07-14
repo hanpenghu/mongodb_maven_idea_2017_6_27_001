3b152aadb7fef78e997816992b88174ed28cd56a
hanpenghu <563909408@qq.com>
2017-07-14 09:29:37 +0800

    2017_7_14   weekday(5)   9:29:33

---
+++ src/test/java/utils/MongodbConnectionsUtils/IpAndPort.java
@@ -0,0 +1,34 @@
+package utils.MongodbConnectionsUtils;
+
+/**
+ * Created by Administrator on 2017-07-13.
+ */
+public class IpAndPort {
+    private String ip;
+    private Integer port;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("com.hanhan.test.utils.MongodbConnectionsUtils.IpAndPort{");
+        sb.append("ip='").append(ip).append('\'');
+        sb.append(", port=").append(port);
+        sb.append('}');
+        return sb.toString();
+    }
+}
